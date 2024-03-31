7e35258df597b4770456c190ccfc72a2f7fb02e7
Gusthavo Alencar <56687423+gusthavoalencar@users.noreply.github.com>
2024-03-31 17:50:24 +0100

    eslint+prettier & fixed eslint errors

--- backend/controllers/itemControllers/itemControllers.ts
+++ backend/controllers/itemControllers/itemControllers.ts
@@ -1,24 +1,24 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
 const itemControllers = {
   getItems: (req: Request, res: Response) => {
-    let items = { item: 'bread' };
+    const items = { item: "bread" };
     try {
       return res.status(200).json(items);
-    } catch (error: any) {
-      return res.status(400).json({ error: error.message });
+    } catch (error) {
+      return res.status(400).json({ error: (error as Error).message });
     }
   },
 
   getHelloWorld: (req: Request, res: Response) => {
-    let items = { test: 'hello world' };
-    console.log('hello woeld');
+    const items = { test: "hello world" };
+    console.log("hello woeld");
     try {
       return res.status(200).json(items);
-    } catch (error: any) {
-      return res.status(400).json({ error: error.message });
+    } catch (error) {
+      return res.status(400).json({ error: (error as Error).message });
     }
-  },
+  }
 };
 
 export default itemControllers;
--- backend/server.ts
+++ backend/server.ts
@@ -1,30 +1,25 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import ItemsRouter from './routes/Items';
+import * as dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import ItemsRouter from "./routes/Items";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    next();
-  }
-);
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  next();
+});
 
-const allowedOrigins = [
-  `http://localhost:${process.env.PORT}`,
-  `http://localhost:${process.env.FRONTEND_PORT}`,
-];
+const allowedOrigins = [`http://localhost:${process.env.PORT}`, `http://localhost:${process.env.FRONTEND_PORT}`];
 
 const options: cors.CorsOptions = {
-  origin: allowedOrigins,
+  origin: allowedOrigins
 };
 app.use(cors(options));
 
-app.use('/api/items', ItemsRouter);
+app.use("/api/items", ItemsRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
